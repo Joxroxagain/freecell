f9bf2fb326347f05618d8ff11885cb533a0630b4
francis <francisz20@mail.wlu.edu>
2018-11-11 21:02:10 -0500

    Fixed Game class added loops for the clear and toString methods

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Represents the creation of a Free Cell card game and 
  * provides logic and functionality for the game
@@ -9,64 +11,31 @@ public class Game {
 
 	private Deck deck;
 	
-	private Tableau tableau1;
-	private Tableau tableau2;
-	private Tableau tableau3;
-	private Tableau tableau4;
-	private Tableau tableau5;
-	private Tableau tableau6;
-	private Tableau tableau7;
-	private Tableau tableau8;
-	
-	private FreeCell freeCell1;
-	private FreeCell freeCell2;
-	private FreeCell freeCell3;
-	private FreeCell freeCell4;
-	
-	private HomeCell homeCell1;
-	private HomeCell homeCell2;
-	private HomeCell homeCell3;
-	private HomeCell homeCell4;
+	private static Tableau[] tableaux = new Tableau[8];
+	private static FreeCell[] freeCells = new FreeCell[4];
+	private static HomeCell[] homeCells = new HomeCell[4];
 
 	public Game() {
 	    // Create deck and empty tableau cells
 		deck = new Deck();
-		tableau1 = new Tableau();
-		tableau2 = new Tableau();
-		tableau3 = new Tableau();
-		tableau4 = new Tableau();
-		tableau5 = new Tableau();
-		tableau6 = new Tableau();
-		tableau7 = new Tableau();
-		tableau8 = new Tableau();
+		Arrays.fill(tableaux, new Tableau());
+		// Create the four free cells
+		Arrays.fill(freeCells, new FreeCell());
+		// Create four home cells
+		Arrays.fill(homeCells, new HomeCell());
 		
 		// Shuffle the deck
 		deck.shuffle();
 		
 		// Deal the cards to empty tableaux
+		int i = 0 % 8;
+		int mod = i % 8;
 		while(!deck.isEmpty()) {
-			tableau1.add(deck.deal());
-			tableau2.add(deck.deal());
-			tableau3.add(deck.deal());
-			tableau4.add(deck.deal());
-			if (deck.isEmpty()) break;
-			tableau5.add(deck.deal());
-			tableau6.add(deck.deal());
-			tableau7.add(deck.deal());
-			tableau8.add(deck.deal());	
+			tableaux[mod].add(deck.deal());
+			i++;
+			mod = i % 8; 
 		}
-		
-		// Create the four free cells
-		freeCell1 = new FreeCell();
-		freeCell2 = new FreeCell();
-		freeCell3 = new FreeCell();
-		freeCell4 = new FreeCell();
-		
-		// Create four home cells
-		homeCell1 = new HomeCell();
-		homeCell2 = new HomeCell();
-		homeCell3 = new HomeCell();
-		homeCell4 = new HomeCell();
+	
 		
 	}
 
@@ -85,72 +54,40 @@ public class Game {
 	public void reset() {
 		deck = new Deck();
 		deck.shuffle();
-		tableau1.clear();
-		tableau2.clear();
-		tableau3.clear();
-		tableau4.clear();
-		tableau5.clear();
-		tableau6.clear();
-		tableau7.clear();
-		tableau8.clear();
-		freeCell1.clear();
-		freeCell2.clear();
-		freeCell3.clear();
-		freeCell4.clear();
-		homeCell1.clear();
-		homeCell2.clear();
-		homeCell3.clear();
-		homeCell4.clear();
+		
+	    for(int i = 0; i < 8; i++) {
+	    	tableaux[i].clear();
+	    	    }
+	    for(int i = 0; i < 4; i++) {
+	    	freeCells[i].clear();
+	    	homeCells[i].clear();
+	    	    }
 		
 		// Deal the cards to empty tableaux
+		int i = 0 % 8;
+		int mod = i % 8;
 		while(!deck.isEmpty()) {
-			tableau1.add(deck.deal());
-			tableau2.add(deck.deal());
-			tableau3.add(deck.deal());
-			tableau4.add(deck.deal());
-			if (deck.isEmpty()) break;
-			tableau5.add(deck.deal());
-			tableau6.add(deck.deal());
-			tableau7.add(deck.deal());
-			tableau8.add(deck.deal());	
+			tableaux[mod].add(deck.deal());
+			i++;
+			mod = i % 8; 
 		}
 	}
 	
 	@Override
 	public String toString() {
 
-		return "\nTableau 1\n" 
-				+ tableau1.toString()
-				+ "\nTableau 2\n"
-				+ tableau2.toString()
-				+ "\nTableau 3\n"
-				+ tableau3.toString()
-				+ "\nTableau 4\n" 
-				+ tableau4.toString()
-				+ "\nTableau 5\n" 
-				+ tableau5.toString()
-				+ "\nTableau 6\n" 
-				+ tableau6.toString()
-				+ "\nTableau 7\n" 
-				+ tableau7.toString()
-				+ "\nTableau 8\n" 
-				+ tableau8.toString();
+		String s = "";
+		
+		for(int i = 0; i < 8; i++) {
+			s += tableaux[i];
+		}
+		
+		return s;
+		
 	}
 	
 	
-	// For testing in the tester class
-	public Tableau getTableau1() {
-		return tableau1;
-	}
-	public Tableau getTableau2() {
-		return tableau1;
-	}
-	public FreeCell getFreeCell1() {
-		return freeCell1;
-	}	
-	public HomeCell getHomeCell1() {
-		return homeCell1;
-	}
+
 }
 // hasWinner
 //hasLoser
