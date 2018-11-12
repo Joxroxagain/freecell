e4f6b22d3a16ef21685028c585cf1b3055ee0201
Joe Scott <jox.rox.js@gmail.com>
2018-11-11 21:27:52 -0500

    added panel framework

---
+++ src/SingalPanel.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics;
+
+public class SingalPanel extends AbstractPanel {
+
+	
+	@Override
+    public void paintComponent(Graphics g){
+		
+    }
+	
+}
---
+++ src/StackedPanel.java
@@ -0,0 +1,6 @@
+
+public class StackedPanel extends AbstractPanel {
+
+	
+	
+}
