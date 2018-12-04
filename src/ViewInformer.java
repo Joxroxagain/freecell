997b1f3849609c88b8ae92141c0333233eaeb66d
francis <francisz20@mail.wlu.edu>
2018-12-03 19:32:49 -0500

    added ViewInformer

---
+++ src/ViewInformer.java
@@ -0,0 +1,13 @@
+
+public interface ViewInformer {
+	
+	
+public void panelPressed(Cell panel);
+/**
+ * returns the panel that's pressed
+ * @param SinglePane panel 
+ * @return selected panel
+ * 
+ */
+
+}
